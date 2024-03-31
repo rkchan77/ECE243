5408d6525245f953563e845efb6c2209d8a9e828
maishimozato <138684155+maishimozato@users.noreply.github.com>
2024-03-31 11:15:12 -0400

    added code for character buffer and writing letters onto VGA

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "nios2_ctrl_reg_macros.h"
+#include "address_map_nios2.h"
 #include "interrupts.h"
 
 void plot_pixel(int x, int y, short int line_color);
@@ -51,4 +52,18 @@ void plot_pixel(int x, int y, short int line_color) {
   volatile short int* one_pixel_address;
   one_pixel_address = pixel_buffer_start + (y << 10) + (x << 1);
   *one_pixel_address = line_color;
+}
+
+/* subroutine to draw a string of text onto the VGA 
+ for chacaters, the entire screen is represented as a rectangular grid of 80 cols (x) x 60 rows (y) */
+void draw_text(int x, int y, char* text_ptr){
+  volatile char *character_buffer = (char*)FPGA_CHAR_BASE;
+
+  /* each character is 1 byte in memory so to find determine the location, add {x,y} offset to the base address*/
+  int offset = (y << 7) + x;
+  while(*(text_ptr)){
+    *(character_buffer + offset) = *(text_ptr);
+    ++text_ptr;
+    ++offset;
+  }
 }
